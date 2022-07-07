ca6b8f570e2e7c23994fecf9ae0cd749f207eb1a
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2022-07-07 15:46:45 +0530

    added dependency handler to module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { startServer } = require('./src/server/server.js');
 const { createRouter } = require('./src/server/asyncRouter.js');
 const { createCache } = require('./src/createCache.js');
 const { parseUrl } = require('./src/handlers/parseUrl.js');
+const { dependencyHandler } = require('./src/handlers/dependencyHandler.js');
 const { injectCookie } = require('./src/handlers/cookiesHandler.js');
 const { notFoundHandler } = require('./src/handlers/notFoundHandler.js');
 const { loginHandler } = require('./src/handlers/loginHandler.js');
@@ -11,5 +12,5 @@ const { protectedHandler } = require('./src/handlers/protectedHandler.js');
 const { injectSession } = require('./src/handlers/sessionHandler.js');
 
 module.exports = {
-  startServer, createRouter, createCache, parseUrl, notFoundHandler, injectCookie, loginHandler, logoutHandler, parseBodyParams, protectedHandler, injectSession
+  startServer, createRouter, createCache, parseUrl, notFoundHandler, injectCookie, loginHandler, logoutHandler, parseBodyParams, protectedHandler, injectSession, dependencyHandler
 };
